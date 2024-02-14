45a69793f1a99bbfb213a185828db07a767c4702
Jet Vergara <59007045+Kurumi0343@users.noreply.github.com>
2024-02-14 18:20:25 +0800

    Add files via upload

--- music.js
+++ music.js
@@ -1,11 +1,18 @@
-let audioInitialized = false;
 
-window.addEventListener('click', () => {
-  if (!audioInitialized) {
-    const audio = new Audio('files/music.mp3');
-    audio.autoplay = true;
-    audio.loop = true;
-    document.body.appendChild(audio);
-    audioInitialized = true;
-  }
-});
+function yes() {
+  document.getElementById("mydialog").hidden = true;
+  const audio = new Audio('files/music.mp3');
+  audio.autoplay = true;
+  audio.loop = true;
+  document.body.appendChild(audio);
+  audioInitialized = true;
+}
+
+function no() {
+  if (document.getElementById("clickno").innerHTML == 'Click "Yes" Please') {
+    document.getElementById("clickno").innerHTML = "Please🥺"
+  } else if (document.getElementById("clickno").innerHTML == 'Please🥺') {
+    document.getElementById("noButton").hidden = true;
+    document.getElementById("clickno").innerHTML = "Now you can't click no😎"
+  };
+}
